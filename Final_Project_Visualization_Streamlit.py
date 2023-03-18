a7d429b49c8f7e184c8b50c750b9f40ab24e0a8c
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2023-03-18 11:51:30 +0200

    Update Final_Project_Visualization_Streamlit.py

--- Final_Project_Visualization_Streamlit.py
+++ Final_Project_Visualization_Streamlit.py
@@ -3,7 +3,7 @@ import warnings
 warnings.filterwarnings('ignore')
 import numpy as np
 import pandas as pd   
-import matplotlib.pyplot as plt
+
 import seaborn as sns
 from imblearn.over_sampling import SMOTE
 import itertools
