79b83949fbdf61c75b02e8afacfd2cca86c958b2
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2023-03-18 18:58:39 +0200

    Update Final_Project_Visualization_Streamlit.py

--- Final_Project_Visualization_Streamlit.py
+++ Final_Project_Visualization_Streamlit.py
@@ -4,17 +4,20 @@ warnings.filterwarnings('ignore')
 import numpy as np
 import pandas as pd   
 from imblearn.over_sampling import SMOTE
-import itertools
 import streamlit as st
+
+import plotly.graph_objects as go  #6
 import plotly.express as px  #7
+from plotly.subplots import make_subplots  #8
 from sklearn.model_selection import train_test_split  #9 
 
-from scipy import stats #19
+
 from category_encoders import BinaryEncoder #23
 
 
 from sklearn.impute import SimpleImputer  #11
 from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder , MinMaxScaler , StandardScaler, RobustScaler #12
+from sklearn.linear_model import LinearRegression  #13
 from sklearn.model_selection import train_test_split
 
 from sklearn.linear_model import LogisticRegression
@@ -29,6 +32,9 @@ from sklearn.ensemble import RandomForestClassifier
 df = pd.read_csv('Adult.csv', na_values=['N/A', 'no', ' ?','NaN','No info'])
 df.columns  = [i.replace(' ','_') for i in df.columns]
 df.columns  = [i.upper() for i in df.columns]
+df.head(3)
+
+
 
 
 df['TARGET'] = df.TARGET.str.replace('<=50K','0').str.replace('>50K','1')
@@ -43,19 +49,7 @@ df.dropna(axis=0, inplace= True)
 Catego=['WORK_CLASS','MARITAL_STATUS','OCCUPATION','RELATIONSHIP','RACE','SEX','NATIVE_COUNTRY']
 Numero=['AGE','FNLWGT','EDUCATION_NUMBER','CAPITAL_GAIN', 'CAPITAL_LOSS', 'HRS/WEEK']
 
-#fig1=px.histogram(data_frame=df, x=WORK_CLASS)
-#fig2=px.histogram(data_frame=df, x=MARITAL_STATUS)
-#fig3=px.histogram(data_frame=df, x=OCCUPATION)
-#fig4=px.histogram(data_frame=df, x=RELATIONSHIP)
-#fig5=px.histogram(data_frame=df, x=RACE)
-#fig6=px.histogram(data_frame=df, x=SEX)
-#fig7=px.histogram(data_frame=df, x=NATIVE_COUNTRY)
-#fig8=px.histogram(data_frame=df, x=AGE)
-#fig9=px.histogram(data_frame=df, x=EDUCATION_NUMBER)
-#fig10=px.histogram(data_frame=df, x=CAPITAL_GAIN)
-#fig11=px.histogram(data_frame=df, x=CAPITAL_LOSS)
-#fig12=px.histogram(data_frame=df, x=HRS/WEEK)
-#fig13=px.histogram(data_frame=df, x=FNLWGT)
+
 
 for i in [' Cuba', ' Jamaica', ' India', ' Mexico', ' Puerto-Rico',' Honduras' ,' England' ,' Canada', ' Germany', ' Iran' ,' Philippines', ' Poland', ' Columbia', ' Cambodia', ' Thailand', ' Ecuador', ' Laos', ' Taiwan', ' Haiti', ' Portugal', ' Dominican-Republic' ,' El-Salvador',' France' ,' Guatemala', ' Italy', ' China', ' South' ,' Japan', ' Yugoslavia',' Peru', ' Outlying-US(Guam-USVI-etc)' ,'Guam-USVI',' Scotland', ' Trinadad&Tobago',' Greece', ' Nicaragua', ' Vietnam', ' Hong', ' Ireland' ,' Hungary',' Holand-Netherlands']:
     df.NATIVE_COUNTRY=df.NATIVE_COUNTRY.str.replace(i,'Other')
@@ -66,9 +60,6 @@ df.NATIVE_COUNTRY=df.NATIVE_COUNTRY.str.replace(' ','')
 
 
 
-#fig5 = px.imshow(df.corr()) 
-
-
 
 
 
@@ -77,7 +68,6 @@ y = df.TARGET
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 Catego_traino =  X_train.select_dtypes(include=['object']).columns
-
 
 
 
@@ -102,29 +92,6 @@ smote = SMOTE(random_state=32)
 
 X_train_sm, y_train_sm = smote.fit_resample(X_train_enc, y_train)
 
-# Logistic Regression
-# Logistic Regression After SMOTE
-lr = LogisticRegression(random_state=42)
-lr.fit(X_train_sm, y_train_sm)
-y_pred = lr.predict(X_test_enc)
-
-#fig_cm1 = plot_confusion_matrix(lr, X_test_enc, y_test, cmap='Blues', values_format='d')
-
-
-
-rf = RandomForestClassifier(random_state=42)
-rf.fit(X_train_sm, y_train_sm)
-y_pred = rf.predict(X_test_enc)
-
-#fig_cm2= plot_confusion_matrix(rf, X_test_enc, y_test, cmap='Blues', values_format='d')
-
-
-# Feature Importance
-#importances = pd.DataFrame({'feature':X_train_enc.columns, 'importance':np.round(rf.feature_importances_,3)})
-#importances = importances.sort_values('importance', ascending=False).set_index('feature')
-
-
-
 
 # Grid Search
 # Grid Search
@@ -136,41 +103,19 @@ param_grid = {'n_estimators': [100,140,130,120], 'max_depth': [60,50,70]}
 grid = GridSearchCV(rf, param_grid, cv=3, scoring='roc_auc', n_jobs=-1)
 grid.fit(X_train_sm, y_train_sm)
 
-print(grid.best_params_)
-
-
-
 
 rf = RandomForestClassifier(random_state=42, n_estimators=grid.best_params_['n_estimators'], max_depth=grid.best_params_['max_depth'])
 rf.fit(X_train_sm, y_train_sm)
-y_pred = rf.predict(X_test_enc)
+
 
 #fig_cm3=plot_confusion_matrix(rf, X_test_enc, y_test, cmap='Blues', values_format='d')
 
 
-import pickle
-
-# Save the model as a pickle in a file
-
-filename = 'Adult_Model.sav'
-
-pickle.dump(rf, open(filename, 'wb')) 
-
-
-#def predict(user):
-
-    # Load the model from the file
-#    model = pickle.load(open(filename, 'rb'))
-
-    # Make predictions
-#    predictions = model.predict(user)
-
-#    return predictions
     
 st.markdown(" <center>  <h1> Predicting Adult's Annual Salary </h1> </font> </center> </h1> ",
             unsafe_allow_html=True)
 
-
+#im = Image.open("C://Users//hp//Desktop//Data Science//Final Project//50K$.jpg")
 im = Image.open("50K$.jpg")
 image = np.array(im)
 st.image(image)
@@ -186,25 +131,64 @@ Sex=st.selectbox("Select your race",(' Male' ,' Female'))
 Native_Country = st.selectbox("Select your country",('USA' ,'Other'))
  
 Occupation = st.selectbox("Select your occupation",(' Adm-clerical' ,' Exec-managerial',' Handlers-cleaners',' Prof-specialty',' Other-service' ,' Sales' ,' Transport-moving' ,' Farming-fishing',' Machine-op-inspct', ' Tech-support' ,' Craft-repair', ' Protective-serv',' Armed-Forces' ,' Priv-house-serv'))
-
-Capital_Gain = st.number_input('Enter your capital gain') 
-Capital_Loss = st.number_input('Enter your capital loss') 
+ 
+Capital_Gain = st.number_input('Enter the capital gain') 
+Capital_Loss = st.number_input('Enter the capital loss') 
 
 
 Relationship = st.selectbox("Select your relationship status",(' Not-in-family' ,' Husband' ,' Wife', ' Own-child' ,' Unmarried',' Other-relative'))
 Workclass = st.selectbox("Select your workclass",(' State-gov' ,' Self-emp-not-inc', ' Private' ,' Federal-gov', ' Local-gov',' Self-emp-inc' ,' Without-pay' ))
+ 
 Marital_Status =st.selectbox("Select your mzrital status",(' Never-married' ,' Married-civ-spouse', ' Divorced',' Married-spouse-absent' ,' Separated', ' Married-AF-spouse',' Widowed'))
   
-dic = {'AGE': [Age], 'WORK_CLASS': [Workclass],'EDUCATION_NUMBER':[Education_Number], 'MARITAL_STATUS':[Marital_Status] , 'OCCUPATION':[Occupation] , 'RELATIONSHIP':[Relationship] , 'RACE':[Race] , 'SEX':[Sex], 'CAPITAL_GAIN':[Capital_Gain], 'CAPITAL_LOSS':[Capital_Loss], 'HRS/WEEK':[HRS_WK],'NATIVE_COUNTRY':[Native_Country]}
-df1 = pd.DataFrame(dic)
-df1=df1.T
-rf = RandomForestClassifier(random_state=42, n_estimators=grid.best_params_['n_estimators'], max_depth=grid.best_params_['max_depth'])
-rf.fit(X_train_sm, y_train_sm)
-Y = rf.predict(df1)
 
-if Y==1:
+df1 = pd.read_csv('C:/Users/hp/Desktop/Data Science/Final Project/Adult_Ans.csv', na_values=['N/A', 'no', ' ?','NaN','No info'])
+df1.columns  = [i.replace(' ','_') for i in df1.columns]
+df1.columns  = [i.upper() for i in df1.columns]
+
+
+
+df['AGE'].iloc[0]=Age
+df['EDUCATION_NUMBER'].iloc[0]=Education_Number
+df['HRS/WEEK'].iloc[0]=HRS_WK
+df['RACE'].iloc[0]=Race
+df['SEX'].iloc[0]=Sex
+df['NATIVE_COUNTRY'].iloc[0]=Native_Country
+df['OCCUPATION'].iloc[0]=Occupation
+df['CAPITAL_GAIN'].iloc[0]=Capital_Gain
+df['CAPITAL_LOSS'].iloc[0]=Capital_Loss
+df['RELATIONSHIP'].iloc[0]=Relationship
+df['WORK_CLASS'].iloc[0]=Workclass
+df['MARITAL_STATUS'].iloc[0]=Marital_Status
+
+
+Catego1=['WORK_CLASS','MARITAL_STATUS','OCCUPATION','RELATIONSHIP','RACE','SEX','NATIVE_COUNTRY']
+Numero1=['AGE','EDUCATION_NUMBER','CAPITAL_GAIN', 'CAPITAL_LOSS', 'HRS/WEEK']
+
+df1_=encoder.transform(df1[Catego1]) 
+df1_=scaler.transform(df1[Numero])
+
+Y = rf.predict(df1_)
+
+
+if st.button('Calculate'):
+    if Y==1:
     st.write(f'The expected salary would be >50K ')
-else:
+    
+    else: 
     st.write(f'The expected salary would be less than or = 50 K ')
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
